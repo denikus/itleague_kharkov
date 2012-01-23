0f724146101d63d5897aed3ebc3de46152df72f1
denix <denis.soloshenko@gmail.com>
2012-01-23 12:09:21 +0200

    sponsors removed

--- layout/js/partners.js
+++ layout/js/partners.js
@@ -1,7 +1,6 @@
 (function() {
 	var cnt = [
-		'<div class="pblock"><a href="http://itleague.football.kharkov.ua" title="Харьков Футбольный - IT-Лига" target="_blank"><img src="layout/partners/p_s_fk.gif" width="180" height="45" alt="Харьков Футбольный - IT-Лига"/></a></div><div class="pblock"><a href="http://shop.football.kharkov.ua" title="Футбольный Магазин" target="_blank"><img src="layout/partners/p_s_fs.gif" width="180" height="45" alt="Футбольный Магазин"/></a></div>',
-		'<div class="pblock"><a href="http://www.zfort.com.ua/" title="Zfort Group" target="_blank"><img src="layout/partners/zfort.jpg" width="180" height="90" alt="Zfort Group"/></a></div>'
+		'<div class="pblock"><a href="http://itleague.football.kharkov.ua" title="Харьков Футбольный - IT-Лига" target="_blank"><img src="layout/partners/p_s_fk.gif" width="180" height="45" alt="Харьков Футбольный - IT-Лига"/></a></div><div class="pblock"><a href="http://shop.football.kharkov.ua" title="Футбольный Магазин" target="_blank"><img src="layout/partners/p_s_fs.gif" width="180" height="45" alt="Футбольный Магазин"/></a></div>'
 	];
 //	var rand = function(n){return (Math.floor(Math.random()*n+1));};
 //	var p1 = rand(cnt.length)-1;
